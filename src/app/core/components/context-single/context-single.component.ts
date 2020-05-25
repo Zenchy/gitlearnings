58163644a0b5da70cf6cea04f091555f09bc2418
Alvaro Corral <alvaro.corral@innocv.com>
2020-05-25 02:14:37 +0200

    Activated canvas

--- src/app/core/components/context-single/context-single.component.ts
+++ src/app/core/components/context-single/context-single.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core'
 
 @Component({
   selector: 'app-context-single',
   templateUrl: './context-single.component.html',
   styleUrls: ['./context-single.component.scss']
 })
-export class ContextSingleComponent implements OnInit {
+export class ContextSingleComponent implements AfterViewInit {
+
+  @ViewChild('canvas')
+  canvas: ElementRef<HTMLCanvasElement>
+  ctx: CanvasRenderingContext2D
+
   constructor() {}
 
-  ngOnInit() {
-
+  ngAfterViewInit() {
+    this.buildCanvas()
   }
+
+  buildCanvas() {
+    this.ctx = this.canvas.nativeElement.getContext('2d')
+    this.ctx.fillStyle = 'red'
+    this.ctx.fillRect(5, 5, 150, 50)
+  }
+
 }
